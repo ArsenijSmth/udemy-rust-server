baea2f216c43db736678e76d5e9795131fcbbdc9
arsenij <arsenij.smth@gmail.com>
2021-04-12 00:08:12 +0300

    Add QueryString as HashMap

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,8 @@
 pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
+pub use querty_string::{QueryString, Value as QueryStringValue};
 
 pub mod request;
 pub mod method;
+pub mod querty_string;
