9134f51eef78c7ec3d3c36c68a2cd5070304372f
arsenij <arsenij.smth@gmail.com>
2021-04-12 23:34:44 +0300

    Add Response and StatusCode structs

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,8 +3,10 @@ pub use method::Method;
 pub use request::ParseError;
 pub use querty_string::{QueryString, Value as QueryStringValue};
 pub use response::Response;
+pub use status_code::StatusCode;
 
 pub mod request;
 pub mod method;
 pub mod querty_string;
 pub mod response;
+pub mod status_code;
