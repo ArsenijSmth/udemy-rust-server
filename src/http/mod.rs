e51ad80162a06662da30b53480d042e2f12bb745
arsenij <arsenij.smth@gmail.com>
2021-04-12 23:15:22 +0300

    Add derive attribte

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,7 +2,9 @@ pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
 pub use querty_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod request;
 pub mod method;
 pub mod querty_string;
+pub mod response;
