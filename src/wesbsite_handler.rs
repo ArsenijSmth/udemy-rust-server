9d1facd0ee1a7488785a4c55a259bd6490b9fc7e
arsenij <arsenij.smth@gmail.com>
2021-04-14 10:10:51 +0300

    Add matching for routes. Add env

--- src/wesbsite_handler.rs
+++ src/wesbsite_handler.rs
@@ -1,10 +1,25 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    publict_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(publict_path: String) -> Self {
+        Self { publict_path }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Hi from Handler</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
